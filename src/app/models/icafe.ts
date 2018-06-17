3ae1d3ae91080403a456658105fa74b1b2e4789c
shubhojit <royshubhojit@gmail.com>
2018-06-18 03:15:49 +1000

    Aligning towards cafe display.

---
+++ src/app/models/icafe.ts
@@ -0,0 +1,8 @@
+export interface ICafe {
+  cafeID: number;
+  cafeName: string;
+  owner: string;
+  openTime: string;
+  closeTime: string;
+  dayOfTheWeek: string;
+}
--- src/app/templates/welcome/welcome.component.ts
+++ src/app/templates/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { pageTransition } from '../../shared/animations';
+import { ICafe } from '../../models/icafe';
 
 @Component({
   selector: 'cd-welcome',
@@ -12,6 +13,50 @@ export class WelcomeComponent implements OnInit {
   pageTitle = 'Date Selection';
 
   state = 'in';
+
+  cafes: ICafe[] = [
+    {
+      cafeID: 1,
+      cafeName: 'CBD coffee shop',
+      owner: 'Jhon Doe',
+      openTime: '07:00 am',
+      closeTime: '03:00 pm',
+      dayOfTheWeek: 'Monday - Friday'
+    },
+    {
+      cafeID: 2,
+      cafeName: 'CBD shop of Coffeee',
+      owner: 'Jane Bloggs',
+      openTime: '07:00 am',
+      closeTime: '03:00 pm',
+      dayOfTheWeek: 'Monday - Wednesday'
+    },
+    {
+      cafeID: 3,
+      cafeName: 'Casa de Cafe',
+      owner: 'Alehandro Seneior',
+      openTime: '07:00 am',
+      closeTime: '11:00 am',
+      dayOfTheWeek: 'Monday - Tuesday'
+    },
+    {
+      cafeID: 4,
+      cafeName: 'Coffeee Superior',
+      owner: 'Clark Gabel',
+      openTime: '07:00 am',
+      closeTime: '01:00 pm',
+      dayOfTheWeek: 'Monday - Wednesday'
+    },
+    {
+      cafeID: 5,
+      cafeName: 'Manana Cafe',
+      owner: 'Sid Butler',
+      openTime: '10:00 am',
+      closeTime: '04:00 pm',
+      dayOfTheWeek: 'Monday - Friday'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
